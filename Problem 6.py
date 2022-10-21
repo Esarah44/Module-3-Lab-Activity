9397db9e2c48185e1d79bcc90ddf894c1c2d3a08
annie-82 <92351283+annie-82@users.noreply.github.com>
2022-10-21 13:35:31 -0500

    Update Problem 6.py

--- Problem 6.py
+++ Problem 6.py
@@ -10,12 +10,15 @@ begin = 1000
 interest = 0.05
 
 #Below three lines will calculate the years with the interest.
-year1 = float(begin)*(1+float(interest))
+
+#you can get a balance B after N years with interest rate R, initial balance P => B = P* (1 + N*R)
+#ONE, TWO, THREE = 1, 2, 3 
+year1 = float(begin)*(1+float(interest))   #year1 = begin * (1 + interest * ONE)
 year2 = float(year1)*(1+float(interest))
 year3 = round(float(year2)*(1+float(interest)),2)
 
 #Below three lines will print the sum of each year plus the interest rate.
-print('The balance after first year is: $' + str(year1) + '.')
+print('The balance after first year is: $' + str(year1) + '.') #print("The balance after first year is $ {}.".format(year1))
 print('The balance after second year is: $' + str(year2) + '.')
 print('The balance after third year is: $' + str(year3) + '.')
 
