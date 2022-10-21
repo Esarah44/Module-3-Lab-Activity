20746582d10975d9e3f60d51232c303589f271c7
annie-82 <92351283+annie-82@users.noreply.github.com>
2022-10-21 13:31:46 -0500

    Update Problem 5.py

--- Problem 5.py
+++ Problem 5.py
@@ -4,13 +4,13 @@
 #The purpose of this program is to greet the user using their name
 
 #Below ask the user to enter the number of miles driven.
-miles = input('Enter number of miles driven: ')
+miles = input('Enter number of miles driven: ')  #miles = float(input("Miles drive: "))
 
 #Below ask user to enter the number of gallons used.
-gas = input('Enter number of gallons used: ')
+gas = input('Enter number of gallons used: ')   #gas = float(input("Gas consumed :"))
 
 #Belwo calculates the miles per gas.
-mpg = int(miles)/int(gas)
+mpg = int(miles)/int(gas)              
 
 #Below prints the output of the miles per gas.
 print('The MPG (Miles per Gas) is ' + str(mpg) + '.')
