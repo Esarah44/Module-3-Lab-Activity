577b489a523b3e55179d3425c2cb2c81292057b4
annie-82 <92351283+annie-82@users.noreply.github.com>
2022-10-21 13:30:36 -0500

    Update Problem 4.py

--- Problem 4.py
+++ Problem 4.py
@@ -4,10 +4,11 @@
 #The purpose of this program is to greet the user using their name
 
 #Below ask the user to enter the radius.
-radius = input('Enter radius:')
+radius = input('Enter radius:')     # You can do it at once: radius = int(input("Enter a radius: "))
 
 #Below will calculate the area of the circle.
 circle_area = 3.1416*(int(radius)**2)
 
 #Below will print the area of the circle.
 print('The area of a circle with radius, ' + str(radius) + ', is ' + str(circle_area) + '.')
+#using a format: print("The area of a circle with radius {} is {}.".format(radius, area))
